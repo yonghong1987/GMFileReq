898a39f1117584879c12cef59ccc43abd71a0eb0
chenyh <chenyonghong2@gome.com.cn>
2019-11-21 17:19:04 +0800

    Initial Commit

---
+++ GMFileReq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GMFileReq
+//
+//  Created by chenyh on 11/21/19.
+//  Copyright © 2019 Gome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
